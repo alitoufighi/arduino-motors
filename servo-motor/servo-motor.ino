a83f1db3e440e75aa77b5f3eaca6fd7d4e2e85c4
Mohammad Ali Toufighi <alitoumob@gmail.com>
2019-06-02 13:06:14 +0430

    add hex files

--- servo-motor/servo-motor.ino
+++ servo-motor/servo-motor.ino
@@ -1,39 +1,43 @@
-#define clockwiseButton A0
-#define antiClockwiseButton A1
-#include <SoftwareSerial.h>
+//#define clockwiseButton A0
+//#define antiClockwiseButton A1
+//#include <SoftwareSerial.h>
 #include <Servo.h>
-#define servoPin 6
+#define servoPin 10
 
-SoftwareSerial vSerial(12, 13);
+//SoftwareSerial vSerial(12, 13);
 Servo servo;  // our servo motor has range -180 and +180.
 
 int pos;
 
 void setup() {
-  pos = 90;
-  vSerial.begin(9600);
+  pos = 0;
+//  vSerial.begin(9600);
   servo.attach(servoPin);
-  pinMode(clockwiseButton, INPUT);
-  pinMode(antiClockwiseButton, INPUT);
+//  pinMode(clockwiseButton, INPUT);
+//  pinMode(antiClockwiseButton, INPUT);
   servo.write(pos);
 }
 
 void loop() {
-  if(digitalRead(clockwiseButton) == HIGH){
-    vSerial.println("Clockwise");
-    pos += 1;
-    pos = pos % 180;
-    servo.write(pos);
-  }
-  while(digitalRead(clockwiseButton) == HIGH);
+//  if(digitalRead(clockwiseButton) == HIGH){
+//    vSerial.println("Clockwise");
+//    pos += 1;
+//    pos = pos % 180;
+//    servo.write(pos);
+//  }
+//  while(digitalRead(clockwiseButton) == HIGH);
+//  
+//  if(digitalRead(antiClockwiseButton) == HIGH){
+//    vSerial.println("Anti Clockwise");
+//    pos -= 1;
+//    pos = pos % 180;
+//    servo.write(pos);
+//  }
+//  while(digitalRead(antiClockwiseButton) == HIGH);
+
+  if(pos < 180) {
+    pos += 5;
+  } else pos = 0;
   
-  if(digitalRead(antiClockwiseButton) == HIGH){
-    vSerial.println("Anti Clockwise");
-    pos -= 1;
-    pos = pos % 180;
-    servo.write(pos);
-  }
-  while(digitalRead(antiClockwiseButton) == HIGH);
-  
-  delay(20);
+  delay(100);
 }
